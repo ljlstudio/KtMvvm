ddc270586a93493bc8238c59c742da130d097b54
lijialun <lijialun@rayvision.com>
2022-08-16 14:00:44 +0800

    #jetpack-增加CameraX 触摸后隐藏所有pop

--- demo/src/main/java/com/kt/ktmvvm/inner/CameraExtensionModeType.kt
+++ demo/src/main/java/com/kt/ktmvvm/inner/CameraExtensionModeType.kt
@@ -15,16 +15,34 @@ import androidx.annotation.IntDef
 annotation class CameraExtensionModeType {
 
     companion object {
+        /**
+         * 无
+         */
         const val NONE = 0
 
+        /**
+         * 焦外成像
+         */
         const val BOKEH = 1
 
+        /**
+         * HDR高清
+         */
         const val HDR = 2
 
+        /**
+         * 夜景模式
+         */
         const val NIGHT = 3
 
+        /**
+         * 脸部修复
+         */
         const val FACE_RETOUCH = 4
 
+        /**
+         * 自动
+         */
         const val AUTO = 5
     }
 
